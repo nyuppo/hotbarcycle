0a20f59e1bd3dc310680cc50862a911289a55d5f
Estecka <estecka@galacsys.com>
2023-10-03 03:10:51 +0200

    Working hotbar cycle.

--- src/main/java/com/github/nyuppo/mixin/RepeatClickCycleMixin.java
+++ src/main/java/com/github/nyuppo/mixin/RepeatClickCycleMixin.java
@@ -1,9 +1,12 @@
 package com.github.nyuppo.mixin;
 
 import com.github.nyuppo.HotbarCycleClient;
+import com.github.nyuppo.HotbarCycleClient.Direction;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.option.GameOptions;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,6 +14,7 @@ import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
@@ -29,5 +33,20 @@ public class RepeatClickCycleMixin {
         if (HotbarCycleClient.getConfig().getRepeatSlotToCycle() && this.options.hotbarKeys[this.player.getInventory().selectedSlot].wasPressed()) {
             HotbarCycleClient.shiftSingle(((MinecraftClient)(Object)this), this.player.getInventory().selectedSlot, HotbarCycleClient.Direction.DOWN);
         }
+    }
+
+    @Redirect(method="doItemPick", at=@At(value="INVOKE", target="net/minecraft/entity/player/PlayerInventory.getSlotWithStack (Lnet/minecraft/item/ItemStack;)I"))
+    private int cyclePickedItem(PlayerInventory inventory, ItemStack pickedItem) {
+        int slot = inventory.getSlotWithStack(pickedItem);
+        if (slot < 0)
+            return slot;
+
+        int x = slot % 9;
+        int y = slot / 9;
+
+        for (int i=y; 0<i; --i)
+            HotbarCycleClient.shiftRows((MinecraftClient)(Object)this, Direction.DOWN);
+
+        return x;
     }
 }
